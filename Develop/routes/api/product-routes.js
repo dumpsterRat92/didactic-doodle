437e72fe1996de90edb74b6f720435c8a947e5f3
dumpsterRat92 <you@example.com>
2024-05-29 16:45:45 -0700

    added product tags and category routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,19 +4,51 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const products = await Product.findAll({
+      include: [
+        {model:Tag,
+        attributes: ['tag_name'],
+        through: { attributes: [] }},
+        {model:Category,
+        attributes: ['category_name']}
+      ]
+    });
+    res.status(200).json(products)
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const product = await Product.findByPk(req.params.id, {
+      include: [
+        {model:Tag,
+        attributes: ['tag_name'],
+        through: { attributes: [] }},
+        {model:Category,
+        attributes: ['category_name']}
+      ]
+    });
+    if (!product) {
+      res.status(404).json({message: 'Product not found'});
+    } else {
+      res.status(200).json(product);
+    }
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -92,8 +124,19 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const product = await Product.findByPk(req.params.id);
+    if(!product){
+      res.status(404).json({message: 'Product not found'});
+    } else {
+      product.destroy();
+      res.status(200).json({message: 'Product deleted successfully'});
+    }
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
